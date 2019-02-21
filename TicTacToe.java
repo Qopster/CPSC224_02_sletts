2d338e4321cd4a10ba438b93e5e38b43516b99ac
Sean Letts <qopster@gmail.com>
2019-02-21 13:18:03 -0800

    The 3x3 works now and changes with (x)'s and (o)'s!

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,5 +1,6 @@
 import javax.swing.*; // Needed for Swing classes
 import java.awt.*;    // Needed for BorderLayout class
+import java.awt.event.*;
 
 /**
    This class demonstrates the BorderLayout manager.
@@ -40,6 +41,8 @@ public class TicTacToe extends JFrame
 	private JButton button7;    // Button 7
 	private JButton button8;    // Button 8
 	private JButton button9;    // Button 9
+	
+	public int turn = 1;
 
 	/**
 		Constructor
@@ -76,41 +79,50 @@ public class TicTacToe extends JFrame
 		SC = new JPanel();
 		SE = new JPanel();
 		
-		JButton button1 = new JButton("NW");
+		JButton button1 = new JButton("");
 		button1.setPreferredSize(new Dimension(40,40));
 		button1.setFont(new Font("Arial", Font.BOLD, 24));
+		button1.addActionListener(new ButtonListener());
 		
-		JButton button2 = new JButton("NC");
+		JButton button2 = new JButton("");
 		button2.setPreferredSize(new Dimension(40,40));
 		button2.setFont(new Font("Arial", Font.BOLD, 24));
+		button2.addActionListener(new ButtonListener());
 		
-		JButton button3 = new JButton("NE");
+		JButton button3 = new JButton("");
 		button3.setPreferredSize(new Dimension(40,40));
 		button3.setFont(new Font("Arial", Font.BOLD, 24));
+		button3.addActionListener(new ButtonListener());
 		
-		JButton button4 = new JButton("CW");
+		JButton button4 = new JButton("");
 		button4.setPreferredSize(new Dimension(40,40));
 		button4.setFont(new Font("Arial", Font.BOLD, 24));
+		button4.addActionListener(new ButtonListener());
 		
-		JButton button5 = new JButton("CC");
+		JButton button5 = new JButton("");
 		button5.setPreferredSize(new Dimension(40,40));
 		button5.setFont(new Font("Arial", Font.BOLD, 24));
+		button5.addActionListener(new ButtonListener());
 		
-		JButton button6 = new JButton("CE");
+		JButton button6 = new JButton("");
 		button6.setPreferredSize(new Dimension(40,40));
 		button6.setFont(new Font("Arial", Font.BOLD, 24));
+		button6.addActionListener(new ButtonListener());
 		
-		JButton button7 = new JButton("SW");
+		JButton button7 = new JButton("");
 		button7.setPreferredSize(new Dimension(40,40));
 		button7.setFont(new Font("Arial", Font.BOLD, 24));
+		button7.addActionListener(new ButtonListener());
 		
-		JButton button8 = new JButton("SC");
+		JButton button8 = new JButton("");
 		button8.setPreferredSize(new Dimension(40,40));
 		button8.setFont(new Font("Arial", Font.BOLD, 24));
+		button8.addActionListener(new ButtonListener());
 		
-		JButton button9= new JButton("SE");
+		JButton button9= new JButton("");
 		button9.setPreferredSize(new Dimension(40,40));
 		button9.setFont(new Font("Arial", Font.BOLD, 24));
+		button9.addActionListener(new ButtonListener());
 		
 		add(button1);
 		add(button2);
@@ -126,8 +138,24 @@ public class TicTacToe extends JFrame
 		// Display the window.
 		//pack();
 		setVisible(true);
+		
 	}
-
+	private class ButtonListener implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			JButton selected = (JButton)e.getSource();
+			if( turn % 2 == 1){
+				selected.setText("X");
+			}
+			else{
+				selected.setText("O");
+			}
+			turn++;
+		}
+		
+	}
+	
 	/**
 		The main method creates an instance of the TicTacToe
 		class, causing it to display its window.
